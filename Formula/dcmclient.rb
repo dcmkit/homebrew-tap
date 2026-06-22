6a2c2dbb2121017c31b931350aab1d3caab09f02
Fuli Wu <fuliwu@msn.com>
2026-06-22 20:10:51 +0800

    dcmclient 0.3.0

--- Formula/dcmclient.rb
+++ Formula/dcmclient.rb
@@ -6,28 +6,28 @@
 class Dcmclient < Formula
   desc "DICOM tools as a command-line toolbox and an agent (MCP) surface, in one binary"
   homepage "https://dcmkit.github.io/dcmclient/"
-  version "0.2.0"
+  version "0.3.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/dcmkit/dcmclient/releases/download/v0.2.0/dcmclient-macos-arm64"
-      sha256 "70ab95de69150ce759e7dd8a1cf0aa33c8d8635a892d62f23c3f77dbd1c3acc3"
+      url "https://github.com/dcmkit/dcmclient/releases/download/v0.3.0/dcmclient-macos-arm64"
+      sha256 "cdce9e4e20c9000b1bb33d23b157dab3b448a44350e7dc9a6d1c44463b503ebc"
     end
     on_intel do
-      url "https://github.com/dcmkit/dcmclient/releases/download/v0.2.0/dcmclient-macos-x86_64"
-      sha256 "ee630d187216392a96fe122ab15973fe9f5637cd73af6c8141b920115c389768"
+      url "https://github.com/dcmkit/dcmclient/releases/download/v0.3.0/dcmclient-macos-x86_64"
+      sha256 "4fc4075e73a3bfd24535f7de47cd7c73e58f895c68809457cd16fb5da1ed943a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dcmkit/dcmclient/releases/download/v0.2.0/dcmclient-linux-aarch64"
-      sha256 "589347ebd7b6a4cfbf52189b505a5e750976d4cfe2bb927a73a89d326b250ece"
+      url "https://github.com/dcmkit/dcmclient/releases/download/v0.3.0/dcmclient-linux-aarch64"
+      sha256 "2295313fcff0aa74683aedeece593b04ba400e4f532e0876445ac96053ca8ef2"
     end
     on_intel do
-      url "https://github.com/dcmkit/dcmclient/releases/download/v0.2.0/dcmclient-linux-x86_64"
-      sha256 "5e6b026bb208f294e398d5f8d89fcc1ed1f9e1078379528bb5039b9d11410422"
+      url "https://github.com/dcmkit/dcmclient/releases/download/v0.3.0/dcmclient-linux-x86_64"
+      sha256 "a5ddb2cb069cfd82b56a6fcfe1f85e924adcee72239e6a47c3dcbf697c84e6c4"
     end
   end
 
@@ -36,6 +36,6 @@ class Dcmclient < Formula
   end
 
   test do
-    assert_match "dcmclient 0.2.0", shell_output("#{bin}/dcmclient --version")
+    assert_match "dcmclient 0.3.0", shell_output("#{bin}/dcmclient --version")
   end
 end
